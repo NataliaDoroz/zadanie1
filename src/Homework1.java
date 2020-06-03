ed11002c31d99440ee9fe2f31198c0a41054cb24
NataliaD <natalia123doroz@gmail.com>
2020-06-03 22:37:47 +0200

    Add file homework1 ang .gitignore

---
+++ src/Homework1.java
@@ -0,0 +1,8 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        System.out.println("Uczę");
+        System.out.println("się\n");
+        System.out.println("programowania");
+
+    }
+}
